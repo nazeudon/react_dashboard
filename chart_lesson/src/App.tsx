897b1283f75563c1e9e76bd1a83f128a80f1f32a
nazeudon <nazeudon48112@icloud.com>
2020-10-25 17:17:14 +0900

    01_line_chart

---
+++ chart_lesson/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import LinePlot from "./components/LinePlot";
+
+function App() {
+  return (
+    <div className="App">
+      <LinePlot />
+    </div>
+  );
+}
+
+export default App;
