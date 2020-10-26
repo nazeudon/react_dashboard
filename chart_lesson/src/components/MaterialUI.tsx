eaa05604a5ea6da401d44ecce1e8845b8dbabd3b
nazeudon <nazeudon48112@icloud.com>
2020-10-26 09:07:11 +0900

    07_makeStyles

--- chart_lesson/src/components/MaterialUI.tsx
+++ chart_lesson/src/components/MaterialUI.tsx
@@ -1,20 +1,25 @@
 import React from "react";
-import { Button } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  btnStyle: {
+    background: "green",
+    padding: "3px 50px",
+  },
+});
 
 const MaterialUI: React.FC = () => {
+  const classes = useStyles();
   return (
     <div>
-      <Button variant="contained" color="primary">
-        primary
+      <Button className={classes.btnStyle} variant="contained" color="primary">
+        primary-contained
       </Button>
-      <Button variant="contained" color="secondary">
-        secondary
+      <Button variant="outlined" color="secondary">
+        secondary-outlined
       </Button>
-      <Button variant="contained" color="default">
-        default
-      </Button>
-      <Button variant="contained" color="inherit">
-        inherit
+      <Button variant="text" color="inherit">
+        inherit-text
       </Button>
     </div>
   );
